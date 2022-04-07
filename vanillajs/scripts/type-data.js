e4172ed21da981215eae00934683c3b4346a5a41
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-07 15:12:42 +0800

    material for type data

--- vanillajs/scripts/type-data.js
+++ vanillajs/scripts/type-data.js
@@ -1,3 +1,13 @@
-let iceCream = []
+let number = 5
+let strings = 'Chocolate'
+let booleans = true
+let arrays = []
+let undefineds = undefined
+let nulls = null
 
-console.log(typeof iceCream);
+console.log(typeof number)
+console.log(typeof strings)
+console.log(typeof booleans)
+console.log(typeof arrays)
+console.log(typeof undefined)
+console.log(typeof nulls)
