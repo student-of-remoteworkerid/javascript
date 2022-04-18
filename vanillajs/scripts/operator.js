3b9f1d5f06d7b3418bfd1fc8867bcc85525f50c8
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-18 18:22:24 +0800

    material for operator

---
+++ vanillajs/scripts/operator.js
@@ -0,0 +1,67 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+// Arithmetic Operator
+// const iceCream = 3 + 4
+// const iceCream = 3 * 4
+// const iceCream = 3 / 4
+// const iceCream = 5 % 3
+
+// console.log(iceCream)
+
+// let iceCream = "5";
+
+// console.log(-iceCream) // Unary negotion
+// console.log(+iceCream) // Unary plus
+// console.log(++iceCream) // Increment 
+// console.log(--iceCream) // Decrement
+
+
+// Assignment Operator
+// const iceCream = 5 % 3
+
+// console.log(iceCream)
+
+// Comparison Operator
+// const iceCream = 5 % 3
+// const iceCream = 6 % 3
+// const iceCream = 10 % 6
+// const iceCream = 10 % 4
+
+/*
+// if (iceCream === 2)
+if (iceCream > 2) 
+     console.log(iceCream)
+else 
+     // console.log("iceCream tidak sama dengan 2")
+     console.log("iceCream nilainya lebih rendah dari 2")
+*/
+
+// Logical Operator
+
+/*
+const sayaSukaEsKrim = true
+const sayaSukaHamburger = true
+
+if (sayaSukaEsKrim && sayaSukaHamburger) console.log("saya suka eskrim dan hamburger")
+*/
+
+/*
+const sayaSukaEsKrim = true
+const sayaSukaHamburger = false
+
+if (sayaSukaEsKrim || sayaSukaHamburger) console.log("saya suka eskrim dan hamburger")
+*/
+
+/*
+const sayaSukaEsKrim = 10
+const sayaSukaHamburger = false
+
+console.log(!sayaSukaEsKrim)
+*/
+
+// String operator
+const sayaSukaEsKrim = "ya sangat suka"
+const sayaSukaHamburger = "apalagi yang ini"
+
+// console.log(sayaSukaEsKrim + ", " + sayaSukaHamburger)
+console.log(`${sayaSukaEsKrim}, ${sayaSukaHamburger}`)
